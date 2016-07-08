a0ed2de212199c9037db0121edb03cd736d2b672
rinoale <rinoale@hanmail.net>
2016-07-08 01:39:40 +0000

    change file names

--- server.js
+++ server.js
@@ -1,8 +1,59 @@
-var net = require('net');
+// Load the TCP Library
+net = require('net');
 
-var server = net.createServer(function(socket) {
-  socket.write('Echo server\r\n');
-  socket.pipe(socket);
-})
+// Keep track of the chat clients
+var clients = [];
 
-server.listen(1337);
+// Start a TCP Server
+net.createServer(function (socket) {
+
+  // Identify this client
+  socket.name = socket.remoteAddress + ":" + socket.remotePort 
+
+  // Put this new client in the list
+  clients.push(socket);
+
+  // Handle incoming messages from clients.
+  socket.on('data', function (data) {
+    var json_data = JSON.parse(data);
+    var body = json_data.body;
+
+    switch(json_data.operation) {
+      case 'userinfo':
+        socket.name = body;
+        // Send a nice welcome message and announce
+        socket.write("Welcome " + socket.name + "\n");
+        broadcast(socket.name + " joined the chat\n", socket);
+        break;
+      case 'message':
+        broadcast(socket.name + "> " + body + "\n", socket);
+        break;
+    }
+  });
+
+  // Remove the client from the list when it leaves
+  socket.on('end', function () {
+    clients.splice(clients.indexOf(socket), 1);
+    broadcast(socket.name + " left the chat.\n");
+  });
+
+  socket.on('error', function () {
+    console.log(socket.name+' has been disconnected.');
+    clients.splice(clients.indexOf(socket), 1);
+  });
+  
+  // Send a message to all clients
+  function broadcast(message, sender) {
+    clients.forEach(function (client) {
+      // Don't want to send it to sender
+      // if (client === sender) return;
+      client.write(message);
+    });
+    // Log it to the server output too
+    process.stdout.write(message)
+  }
+
+}).listen(1337);
+
+// Put a friendly message on the terminal of the server.
+console.log("Chat server running at port 1337\n");
