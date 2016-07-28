e6becae767b0e600d704e961175a3e43b8b119fd
rinoale <rinoale@hanmail.net>
2016-07-28 13:56:27 +0000

    remove comment

--- server.js
+++ server.js
@@ -21,55 +21,6 @@ net.createServer(function (socket) {
 
   var userSocket = new UserSocket(socket);
   userSocket.onData();
-
-  // // Handle incoming messages from clients.
-  // socket.on('data', function (data) {
-  //   var json_data = JSON.parse(data);
-  //
-  //   switch(json_data.operation) {
-  //     case 'inform':
-  //       socket.name = json_data.message;
-  //
-  //       message.setNotice('Welcome ' + socket.name);;
-  //       // Send a nice welcome message and announce
-  //       personalcast(JSON.stringify(message), socket);
-  //
-  //       message.setInfo(socket.name, ' joined the chat');
-  //       break;
-  //     case 'chat':
-  //       message.setChat(socket.name, json_data.message);
-  //       break;
-  //   }
-  //   broadcast(JSON.stringify(message), socket);
-  // });
-  //
-  // // Remove the client from the list when it leaves
-  // socket.on('end', function () {
-  //   message.setInfo(socket.name + ' left the chat');
-  //   broadcast(JSON.stringify(message), socket);
-  //   clients.splice(clients.indexOf(socket), 1);
-  // });
-  //
-  // socket.on('error', function () {
-  //   console.log(socket.name+' has been disconnected.');
-  //   clients.splice(clients.indexOf(socket), 1);
-  // });
-  //
-  // // Send a message to all clients
-  // function broadcast(message, sender) {
-  //   clients.forEach(function (client) {
-  //     // Don't want to send it to sender
-  //     // if (client === sender) return;
-  //     client.write(message);
-  //   });
-  //   // Log it to the server output too
-  //   process.stdout.write(message + "\n")
-  // }
-  //
-  // function personalcast(message, socket) {
-  //   socket.write(message);
-  // }
-
 }).listen(1337);
 // Put a friendly message on the terminal of the server.
 console.log("Chat server running at port 1337\n");
